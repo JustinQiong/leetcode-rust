b1c4a9dca9ebcf80b4c535a8184978fd404da499
Qiong Zhou <justin.qiong@gmail.com>
2024-03-20 09:01:34 +0800

    Fix Bug

--- src/2024/mar/303/num_array.rs
+++ src/2024/mar/303/num_array.rs
@@ -13,7 +13,7 @@ struct NumArray {
 impl NumArray {
 
     fn new(nums: Vec<i32>) -> Self {
-        let mut s = vec![0; nums.len() + 1]
+        let mut s = vec![0; nums.len() + 1];
         for (i, &x) in nums.iter().enumerate() {
             s[i + 1] = s[i] + x;
         }
